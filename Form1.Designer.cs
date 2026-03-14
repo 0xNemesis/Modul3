0777e8737cb34106ad9f6ecd901eb9e07301b790
0xNemesis <0xnemesis@protonmail.ch>
2026-03-14 14:20:23 +0700

    Menambahkan tampilan GUI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,16 +32,22 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNilaiAwal = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtHasil = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(105, 107);
+            this.comboBox1.Items.AddRange(new object[] {
+            "Celcius",
+            "Fahrenheit",
+            "Kelvin",
+            "Reamur"});
+            this.comboBox1.Location = new System.Drawing.Point(228, 108);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(80, 24);
             this.comboBox1.TabIndex = 0;
@@ -50,7 +56,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(104, 88);
+            this.label1.Location = new System.Drawing.Point(227, 89);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(81, 16);
             this.label1.TabIndex = 1;
@@ -59,7 +65,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(104, 148);
+            this.label2.Location = new System.Drawing.Point(227, 149);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(82, 16);
             this.label2.TabIndex = 2;
@@ -68,54 +74,73 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(105, 167);
+            this.comboBox2.Items.AddRange(new object[] {
+            "Celcius",
+            "Fahrenheit",
+            "Kelvin",
+            "Reamur"});
+            this.comboBox2.Location = new System.Drawing.Point(228, 168);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(80, 24);
             this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
-            // textBox1
+            // txtNilaiAwal
             // 
-            this.textBox1.Location = new System.Drawing.Point(256, 107);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(207, 22);
-            this.textBox1.TabIndex = 4;
+            this.txtNilaiAwal.Location = new System.Drawing.Point(379, 108);
+            this.txtNilaiAwal.Name = "txtNilaiAwal";
+            this.txtNilaiAwal.Size = new System.Drawing.Size(207, 22);
+            this.txtNilaiAwal.TabIndex = 4;
+            this.txtNilaiAwal.TextChanged += new System.EventHandler(this.txtNilaiAwal_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(253, 88);
+            this.label3.Location = new System.Drawing.Point(376, 89);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "Nilai Awal";
             // 
-            // textBox2
+            // txtHasil
             // 
-            this.textBox2.Location = new System.Drawing.Point(256, 169);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(207, 22);
-            this.textBox2.TabIndex = 6;
+            this.txtHasil.Location = new System.Drawing.Point(379, 170);
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.ReadOnly = true;
+            this.txtHasil.Size = new System.Drawing.Size(207, 22);
+            this.txtHasil.TabIndex = 6;
+            this.txtHasil.TextChanged += new System.EventHandler(this.txtHasil_TextChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(253, 148);
+            this.label4.Location = new System.Drawing.Point(376, 149);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(38, 16);
             this.label4.TabIndex = 7;
             this.label4.Text = "Hasil";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(379, 227);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 8;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvert);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtHasil);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtNilaiAwal);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -133,10 +158,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNilaiAwal;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtHasil;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnConvert;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -26,5 +26,52 @@ namespace Modul3_103022400010
         {
 
         }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNilaiAwal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtHasil_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double nilai = Convert.ToDouble(txtNilaiAwal.Text);
+
+            string dari = comboBox1.SelectedItem.ToString();
+            string ke = comboBox2.SelectedItem.ToString();
+
+            double celcius = 0;
+
+            if (dari == "Celcius")
+                celcius = nilai;
+            else if (dari == "Fahrenheit")
+                celcius = (nilai - 32) * 5 / 9;
+            else if (dari == "Kelvin")
+                celcius = nilai - 273.15;
+            else if (dari == "Reamur")
+                celcius = nilai * 5 / 4;
+
+            double hasil = 0;
+
+            if (ke == "Celcius")
+                hasil = celcius;
+            else if (ke == "Fahrenheit")
+                hasil = (celcius * 9 / 5) + 32;
+            else if (ke == "Kelvin")
+                hasil = celcius + 273.15;
+            else if (ke == "Reamur")
+                hasil = celcius * 4 / 5;
+
+            txtHasil.Text = hasil.ToString();
+        }
     }
 }
